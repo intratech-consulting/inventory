6cbebd6915499e7f4cb113ec7d9032b235a3b40c
LucasDeLil <lucas.de.lil@student.ehb.be>
2024-05-12 13:35:47 +0200

    logs aangepast 2

--- product_publisher.py
+++ product_publisher.py
@@ -6,7 +6,9 @@ import xml.etree.ElementTree as ET
 import pika
 import logging
 
-
+# Setting up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 product_list = []
 
